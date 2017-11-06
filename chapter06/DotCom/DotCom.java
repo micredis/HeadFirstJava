9232bf3b4cea03868d03f14ac4906415a1c86582
micredis <micredis@gmail.com>
2017-11-06 22:48:21 +0200

    Add chapter06 and initial DotCom game classes

---
+++ chapter06/DotCom/DotCom.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class DotCom {
+	ArrayList<String> locationCells;
+	String name;
+	//int numOfHits = 0;
+
+	public void setLocationCells(ArrayList<String> loc) {
+		locationCells = loc;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String checkYourself(String userInput) {
+		String result = "miss";
+		int index = locationCells.indexOf(userInput);
+		if (index >= 0) {
+			locationCells.remove(index);
+			if (locationCells.isEmpty()) {
+				result = "kill";
+			} else {
+				result = "hit";
+			}
+		}
+
+		return result;
+	}
+}
